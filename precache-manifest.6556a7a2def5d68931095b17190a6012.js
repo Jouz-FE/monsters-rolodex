d359752fd4462ae31cb295c0bcdbbf48e0e8c480
Jouz-FE <fr2d2ric.ma11e@gmail.com>
2020-04-27 19:24:39 +0900

    Updates

---
+++ precache-manifest.6556a7a2def5d68931095b17190a6012.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a433329af2ebd7f86f83b0792d1a921d",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "81f86594492335f81c14",
+    "url": "/monsters-rolodex/static/css/main.1aa6bbfd.chunk.css"
+  },
+  {
+    "revision": "926634218f0cd358d7da",
+    "url": "/monsters-rolodex/static/js/2.a6bff23a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.a6bff23a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81f86594492335f81c14",
+    "url": "/monsters-rolodex/static/js/main.9b71db33.chunk.js"
+  },
+  {
+    "revision": "fc1a59712d7f10d18ab0",
+    "url": "/monsters-rolodex/static/js/runtime-main.664c0a0d.js"
+  }
+]);
